e40a62246acdf13c9cf20c2df41f9d6803153803
benjamin <ebenjaminhd11@gmail.com>
2025-10-15 23:45:41 -0500

    fixing register to return access and refresh token

--- backend/gestor/models/user.py
+++ backend/gestor/models/user.py
@@ -3,12 +3,14 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
 
+    username = None
+
     name = models.CharField(max_length=100)
     email = models.EmailField(max_length=255, unique=True)
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de creación')
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
+    REQUIRED_FIELDS = []
 
     def __str__(self):
         return self.email
--- backend/gestor/views/register_user_view.py
+++ backend/gestor/views/register_user_view.py
@@ -1,5 +1,7 @@
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.tokens import RefreshToken
 from gestor.models import User
 from gestor.serializers import UserSerializer
 
@@ -12,3 +14,24 @@ class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+
+        refresh = RefreshToken.for_user(user)
+        access = refresh.access_token
+
+        return Response({
+            "message": "User created successfully",
+            "user": {
+                "id": user.id,
+                "name": user.name,
+                "email": user.email,
+            },
+            "tokens": {
+                "refresh": str(refresh),
+                "access": str(access)
+            }
+        }, status=status.HTTP_201_CREATED)
